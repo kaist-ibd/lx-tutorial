0f69079bc8dfae8b623b3250d2bec929d39a04ad
starry99 <lucky112@kaist.ac.kr>
2023-04-15 05:08:16 +0900

    add text component

--- src/components/text.js
+++ src/components/text.js
@@ -1,8 +1,46 @@
 import * as React from "react";
-
+import * as styles from "./launcher.module.css";
+// import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Section from "../components/section";
+import Button from "../components/button";
+import MarkdownText from "../components/markdown-text";
+import Heading from "../components/heading";
 
 export default function Text({ heading, secondaryHeading, content }) {
-    return (
-      <p>dd</p>
-    );
-  }
+  const launcherContent = content?.[0];
+//   const image = getImage(launcherContent?.image);
+
+  return (
+    <Section>
+      <div className={styles.root}>
+        <div className={styles.content}>
+          <Heading as="h2" className={styles.secondaryHeading}>
+            {secondaryHeading}
+          </Heading>
+          <Heading as="h1" className={styles.heading}>
+            {heading}
+          </Heading>
+          <LauncherContent {...launcherContent} />
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+function LauncherContent({ primaryText, secondaryText, links }) {
+  return (
+    <div>
+      <MarkdownText {...primaryText} />
+      <MarkdownText {...secondaryText} />
+      <div className={styles.buttonContainer}>
+        {links.map((link, i) => (
+          <Button
+            key={link.id}
+            {...link}
+            variant={i === 0 ? "primary" : "secondary"}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/gatsby-theme-landing-page/sections/index.js
+++ src/gatsby-theme-landing-page/sections/index.js
@@ -1,2 +1,3 @@
 export { default as Launcher } from "gatsby-theme-landing-page/src/sections/launcher";
 export { default as Map } from "gatsby-theme-landing-page/src/sections/map";
+export { default as Text } from "../../components/text";
