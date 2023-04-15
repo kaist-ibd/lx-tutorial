d4bce6a6d27994eae825a0111114f39b3ba4a6c7
starry99 <lucky112@kaist.ac.kr>
2023-04-15 14:50:03 +0900

    add imageText component

---
+++ src/components/imageText.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import * as styles from "./imageText.module.css";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Section, MarkdownText, Button, Heading } from "gatsby-theme-landing-page";
+
+export default function ImageText({ heading, secondaryHeading, content }) {
+  return (
+    <Section>
+      <div className={styles.root}>
+        <div className={styles.content}>
+          <Heading as="h2" className={styles.secondaryHeading}>
+            {secondaryHeading}
+          </Heading>
+          <Heading as="h1" className={styles.heading}>
+            {heading}
+          </Heading>
+          {content.map((item) => (
+            <TextImageContent key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+function TextImageContent({ primaryText, image, links }) {
+  return (
+    <div>
+      <div className={styles.root}>
+        <div className={styles.image}>
+          {image && (
+            <GatsbyImage
+              image={getImage(image)}
+              alt={image.title || getText(primaryText)}
+              className={styles.contentImage}
+            />
+          )}
+        </div>
+        <div className={styles.content}>
+          <MarkdownText {...primaryText} />
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        {links && links.map((link, i) => (
+          <Button
+            key={link.id}
+            {...link}
+            variant={i === 0 ? "primary" : "secondary"}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/gatsby-theme-landing-page/sections/index.js
+++ src/gatsby-theme-landing-page/sections/index.js
@@ -1,3 +1,4 @@
 export { default as Launcher } from "gatsby-theme-landing-page/src/sections/launcher";
 export { default as Map } from "gatsby-theme-landing-page/src/sections/map";
 export { default as Text } from "../../components/text";
+export { default as ImageText } from "../../components/imageText";
